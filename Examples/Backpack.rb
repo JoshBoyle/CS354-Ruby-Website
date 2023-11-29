db8d8ea5a0c529cf8317eb8075e8be2dfe5b35a9
BraedenL <braedenlacombe@u.boisestate.edu>
2023-11-28 21:31:47 -0700

    Updates to classes

--- Examples/Backpack.rb
+++ Examples/Backpack.rb
@@ -1,10 +1,17 @@
+# Backpack.rb
+# Author Braeden LaCombe
+# Creates a virtual 'backpack' that holds objects. The pack has a maxiumum size of objects that it can hold
+
+
 class Backpack
+  # Marks the sizing and an initial Hash for the backpack
   def initialize(max_size)
     @max_size = max_size
     @items = Hash.new(0)
   end
 
   def addItem(item, amount)
+    # Checks to make sure the there are enough open slots for the items being added
     if totalItems + amount <= @max_size
       @items[item] += amount
     else
@@ -12,7 +19,9 @@ class Backpack
     end
   end
 
+  # Giving two inputs allows us to only take a partial amount of one of the items in the backpack
   def removeItem(item, amount)
+    #Checks for if we have both a valid item with key? and enough of the item
     if @items.key?(item) && @items[item] >= amount
       @items[item] -= amount
     else
@@ -20,6 +29,7 @@ class Backpack
     end
   end
 
+  # These are what would be a tyipcal 'getter function'
   def checkAmount(item)
     @items[item]
   end
@@ -28,6 +38,7 @@ class Backpack
     @items.values.sum
   end
 
+  # Could be listed as a toString function to show the contents of the pack
   def printBackpack
     puts "Current Backpack:"
     @items.each { |item, amount| puts "#{item}: #{amount}" }
@@ -35,7 +46,7 @@ class Backpack
   end
 end
 
-# Example Usage:
+# Testing and example for the backpack being filled, and exceedings the maxiumum amounts of items to add
 max_bag_size = 30
 backpack = Backpack.new(max_bag_size)
 
--- Examples/FormattingExamples.rb
+++ Examples/FormattingExamples.rb
@@ -18,6 +18,62 @@ greetings("Class")
 "Hi there!".upcase
 #Both will output HI THERE!
 
+# Quotes: While both can be used, there is a small difference between the two
+# Using double quotes allows special characters to be interpreted and translated
+puts "Hello #{name}\n\n"
+
+# Using single quotes will not allow special characters of any kind to be read and used
+puts 'Hello #{name}\n\n'
+
+# When using an array in Ruby there are a variety of useless features that are included
+testSet = [1, 2.0, "three"]
+puts testSet[-1]    #Produces "three"
+# Ease of use methods such as
+testSet.first
+testSet.last
+testSet.length
+
+# Ruby contains what are called Ranges, where the range given will run in the for each style
+(1..10).each{ |number| puts number}
+# These also see a usage in for loops
+for i in 1..10
+  puts "#{i}"
+end
+
+#Or simply call a loop and break it with an if function!
+j = 0
+loop do
+  break if j > 5
+  puts j
+  j += 1
+end
+
+# The if statement also has some other usefullness to it when used in connection with other functions
+puts "End of time" if x > 10
+
+# Also works with keyword 'unless'
+puts "Keep going" unless x < 10
+
+# Iterators! Have a variety that can be used and modified
+# While
+i = 0
+while i < 5
+  i + 1
+  puts i
+end
+
+# times
+i = 10
+i.times{|i| print i}
+
+# Each
+fruits = %w(mangos bananas apples)
+fruits.each{|unique| print unique}
+
+# Upto
+i = 0
+upperLimit = 10
+i.upto(upperLimit) {|number| print number}
 
 # All three of the following functions will add the two given numbers and return it
 # No matter the differences in how the solution is returned and formatting
@@ -48,6 +104,10 @@ puts "Adding results in #{addSolution} and multiplying gives you #{multiplySolut
 # ';' to run two statements on the same line
 var1 = 3
 var2 = 4; var3 = 5
+
+# We can also do some unique things while assigning and changing values
+var2,var3 = var3,var2
+# Interpreted as 4,5 = 5,4
 
 # Lines can run on using the '\' and does not need to be indented
 # We can also make large block comments by marking them with =begin and =end
