bf113f2f6350c5bdab431574462697be2b6ac3fd
BraedenL <braedenlacombe@u.boisestate.edu>
2023-11-29 00:01:45 -0700

    Operator Comparisons

---
+++ Comparisons/Operators.c
@@ -0,0 +1,25 @@
+int main() {
+    int x = 1;
+    int y = 2;
+    int z;
+
+    z = x + y;
+
+    z = x - y;
+
+    z = x * y;
+
+    z = x / y;
+
+    if(x < y)
+        return;
+    if(x > y)
+        return;
+    if(x <= y)
+        return;
+    if(x >= y)
+        return;
+    if(x == y)
+        return;
+
+}
---
+++ Comparisons/Operators.java
@@ -0,0 +1,27 @@
+package Comparisons;
+
+public class Operators {
+    //Have to create a variable to hold total without modifying x and y
+    int x = 1;
+    int y = 2;
+    int z;
+
+    z = x + y;
+
+    z = x - y;
+
+    z = x * y;
+
+    z = x / y;
+
+    if (x < y)
+        break;
+    if (x > y)
+        break;
+    if (x <= y)
+        break;
+    if (x >= y)
+        break;
+    if (x == y)
+        break;
+}
